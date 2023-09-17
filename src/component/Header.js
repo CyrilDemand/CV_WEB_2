0cf7523b4b8a6194a08c3b6641c8fb738d0a967b
Cyrildemand <cydemand@gmail.com>
2023-09-17 15:14:15 +0200

    menu

--- src/component/Header.js
+++ src/component/Header.js
@@ -123,16 +123,16 @@ function Header(props) {
                 </svg>
                 <div className="menu">
                     <ol>
-                        <li className="home"><a className="not_button" href=""></a></li>
+                        <li onClick={closeMenu} className="home"><a className="not_button" href=""></a></li>
                         <li>
                             {selectLanguage()}
                         </li>
-                        <li className="home"><a className="not_button" href="#home_section">{data.headerHome}</a></li>
-                        <li className="about"><a className="not_button" href="#about_section">{data.headerAboutMe}</a></li>
-                        <li className="projects"><a className="not_button" href="#projects_section">{data.headerProjects}</a></li>
-                        <li className="skills"><a className="not_button" href="#skills_section">{data.headerSkills}</a></li>
-                        <li className="experience"><a className="not_button" href="#experience_section">{data.headerExperience}</a></li>
-                        <li className="contact">
+                        <li onClick={closeMenu} className="home"><a className="not_button" href="#home_section">{data.headerHome}</a></li>
+                        <li onClick={closeMenu} className="about"><a className="not_button" href="#about_section">{data.headerAboutMe}</a></li>
+                        <li onClick={closeMenu} className="projects"><a className="not_button" href="#projects_section">{data.headerProjects}</a></li>
+                        <li onClick={closeMenu} className="skills"><a className="not_button" href="#skills_section">{data.headerSkills}</a></li>
+                        <li onClick={closeMenu} className="experience"><a className="not_button" href="#experience_section">{data.headerExperience}</a></li>
+                        <li onClick={closeMenu} className="contact">
                             <a className="button" href="#contact_section">
                                 <p>{data.headerContact}</p>
                                 <svg className="button_arrow" xmlns="http://www.w3.org/2000/svg"
